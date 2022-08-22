b3caa22c03fcbc814175292159614559236096a8
Edgar Fabiano <eddy.led@gmail.com>
2022-08-22 13:42:16 -0300

    Fixing IllegalArgumentException Targeting S+ (version 31 and above)

--- lib/service/admob_service.dart
+++ lib/service/admob_service.dart
@@ -5,9 +5,8 @@ import 'package:gerador_validador/defaults/defaults_export.dart';
 class AdMobService {
   static final String testAppId = 'ca-app-pub-3940256099942544~3347511713';
 
-  static final String bannerTestAdUnitId = 'ca-app-pub-5932227223136302/1970700133';
-
-  static final String interstitialTestAdUnitId = 'ca-app-pub-3940256099942544/1033173712';
+  static final String interstitialTestAdUnitId =
+      'ca-app-pub-3940256099942544/1033173712';
 
   static final String appId = 'ca-app-pub-5932227223136302~5091687492';
 
@@ -19,15 +18,14 @@ class AdMobService {
   }
 
   static List<String> testDevices() {
-    return ['4B0FDC40963AB3E77AED679FF240F802',
-      '118F4E581E0D5DAA4F78D3B1A29E861C'];
+    return [
+      '4B0FDC40963AB3E77AED679FF240F802',
+      '118F4E581E0D5DAA4F78D3B1A29E861C'
+    ];
   }
 
   static double bannerPadding(BuildContext context) {
-    var height = MediaQuery
-        .of(context)
-        .size
-        .height;
+    var height = MediaQuery.of(context).size.height;
     if (height <= 400) {
       return 32;
     } else if (height >= 720) {
@@ -41,17 +39,10 @@ class AdMobService {
 
   static BannerAd _banner;
 
-  static String _getBannerId() {
-    if (!Constants.isTesting) {
-      return bannerId;
-    }
-    return bannerTestAdUnitId;
-  }
-
   static BannerAd startBanner() {
     if (_banner == null) {
       _banner = BannerAd(
-        adUnitId: _getBannerId(),
+        adUnitId: bannerId,
         size: AdSize.smartBanner,
         targetingInfo: MobileAdTargetingInfo(testDevices: testDevices()),
         listener: (MobileAdEvent event) {
@@ -86,16 +77,9 @@ class AdMobService {
 
   static InterstitialAd get interstitial => _interstitial;
 
-  static String _getInterstitialId() {
-    if (!Constants.isTesting) {
-      return interstitialId;
-    }
-    return interstitialTestAdUnitId;
-  }
-
   static InterstitialAd buildInterstitial() {
     _interstitial = InterstitialAd(
-        adUnitId: _getInterstitialId(),
+        adUnitId: interstitialId,
         targetingInfo: MobileAdTargetingInfo(testDevices: testDevices()),
         listener: (MobileAdEvent event) {
           if (event == MobileAdEvent.loaded) {
